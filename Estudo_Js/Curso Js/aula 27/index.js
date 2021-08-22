37aade76d3fbf14998b26c94cc6755077293700e
Matheus Marques <matheus_marques258@outlook.com>
2021-08-22 18:48:27 -0300

    22/08

---
+++ Estudo_Js/Curso Js/aula 27/index.js
@@ -0,0 +1,9 @@
+// (consicao) ? 'valor para verdadeiro' : 'valor para falso';
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+/* if (pontuacaoUsuario>= 1000) {
+    console.log('Usuário VIP');
+} else {
+    console.log('Usuário normal');
+} */
---
+++ Estudo_Js/Curso Js/aula 32/index.js
@@ -0,0 +1,12 @@
+/* let a = 'A'; // B
+let b = 'B'; // C
+let c = 'C'; // A
+
+const letras = ['B', 'C', 'A'];
+[a, b, c] = letras;
+
+EXERCICIO ANTERIOR  */
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const primeiroNumero = numero[0];
+console.log(numeros);

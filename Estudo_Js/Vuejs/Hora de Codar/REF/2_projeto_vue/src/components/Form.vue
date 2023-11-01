cf9920130d7c97d556befda2f17ee19b16790982
matheus258 <matheus_marques258@outlook.com>
2023-11-01 10:17:19 -0300

    Estudos componentes Vue

---
+++ Estudo_Js/Vuejs/Hora de Codar/REF/2_projeto_vue/src/components/Form.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <form action="">
+            <input type="text"/><br/>
+            <input type="text"/><br/>
+            <input type="text"/><br/>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Form'
+    }
+</script>
---
+++ Estudo_Js/Vuejs/Hora de Codar/REF/2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Estou trabalhando como Analista</p>
+        <p>Utilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>Vue</li>
+            <li>React</li>
+            <li>Node</li>
+        </ul>
+    </div>
+</template>
+
+<script >
+    export default {
+        name: 'Info',
+        data() {
+
+        }
+    }
+</script>

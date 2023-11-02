322777450b259dc0dcf7e9596636e7cda213614c
matheus258 <matheus_marques258@outlook.com>
2023-11-02 10:51:01 -0300

    Dados dinamicos vue

--- Estudo_Js/Vuejs/Hora de Codar/REF/2_projeto_vue/src/components/Info.vue
+++ Estudo_Js/Vuejs/Hora de Codar/REF/2_projeto_vue/src/components/Info.vue
@@ -10,17 +10,24 @@
             <li>Node</li>
         </ul>
         <p v-show="email" >Mande uma mensagem para {{ email }}</p>
+        <p>Para acessar meu portifólio <a v-bind:href="meu_link" target="blank">basta clicar aqui</a></p>
+        <Picture />
     </div>
 </template>
 
 <script >
+    import Picture from './Picture.vue'
     export default {
         name: 'Info',
+        components: {
+            Picture
+        },
         data() {
             return {
                 esta_trabalhando: false,
                 mostrar_email: true,
-                email: 'matheus@gmail.com'
+                email: 'matheus@gmail.com',
+                meu_link: 'https://www.google.com.br/'
             }
         }
     }

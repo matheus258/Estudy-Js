9b397113334540d6cb5cd709df2cf18fcec5963b
matheus258 <matheus_marques258@outlook.com>
2023-11-05 22:23:35 -0300

    Reutilização de componentes

--- Estudo_Js/Vuejs/Hora de Codar/REF/2_projeto_vue/src/components/PrimeiroComponente.vue
+++ Estudo_Js/Vuejs/Hora de Codar/REF/2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -4,17 +4,20 @@
         <h2>DADOS</h2>
         <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
         <MultiplosEventos />
+        <Reutilizacao />
     </div>
 </template>
 
 
 <script>
 import MultiplosEventos from './MultiplosEventos.vue';
+import Reutilizacao from './Reutilizacao.vue';
 
 export default {
     name: 'PrimeiroComponente',
     components: {
-        MultiplosEventos
+        MultiplosEventos,
+        Reutilizacao
     },
     data() {
         return {
---
+++ Estudo_Js/Vuejs/Hora de Codar/REF/2_projeto_vue/src/components/Reutilizacao.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button @click="ativar">Clique Aqui!</button>
+        <br>
+        <span v-show="ativou">Hellow, span ativado</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Reutilizacao',
+    data(){
+        return{
+            ativou: false,
+        }
+    },
+    methods:{
+        ativar(){
+            this.ativou = !this.ativou;
+        }
+    }
+}
+</script>

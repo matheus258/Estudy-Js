b30adae7e09ae31e1a29667786492d7659171745
Matheus <matheusmarquesaraujo53@gmail.com>
2024-01-02 20:48:18 -0300

    Estudos de Callback Js

---
+++ Estudo_Js/HoraDeCodar/44_setTimeoutEsetInterval/js/index.js
@@ -0,0 +1,10 @@
+// setTimeout
+setTimeout(function () {
+  console.log("Olá, eu sou o código executado após 2 segundos!");
+}, 2000);
+
+// setInterval
+const interval = setInterval(() => {
+  let contador = 0;
+  console.log(contador);
+}, 2000);
